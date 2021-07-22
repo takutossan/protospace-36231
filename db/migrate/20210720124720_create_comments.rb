d89b9246812a0f5fd2312e2ea37b03b5314f46d8
takutossan <gikushaku.4989@gmail.com>
2021-07-22 16:03:52 +0900

    外部キー修正1

--- db/migrate/20210720124720_create_comments.rb
+++ db/migrate/20210720124720_create_comments.rb
@@ -2,7 +2,7 @@ class CreateComments < ActiveRecord::Migration[6.0]
   def change
     create_table :comments do |t|
       t.text       :text,         null: false
-      t.references :user_id,      null: false, foreign_key: true
+      t.references :user,         null: false, foreign_key: true
       t.references :prototype_id, null: false, foreign_key: true
       t.timestamps
     end
